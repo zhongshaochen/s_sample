cdd615e84fd817452c1b08c821d0efda5bc0688a
zhongshaochen <zhongshaochen@gmail.com>
2016-04-22 00:30:24 +0000

    Eliminate repetition (solves exercise 3.1)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+    #render template: "static_pages/#{params[:page]}"
   end
 
   def help
+   # render template: "static_pages/#{params[:page]}"
+  end
+  
+  def about
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
+ # get 'static_pages/home' #=> "static_pages#home"
 
-  get 'static_pages/help'
-
+  #get 'static_pages/help'  #=> "static_pages#help"
+  
+  get 'static_pages/about' 
+  
+ # root "static_pages#home", page: "home"
+  
+  #root "static_pages#help", page: "help"
+  
+  root "static_pages#about", page: "about"
 end
