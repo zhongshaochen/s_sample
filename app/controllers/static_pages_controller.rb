1a93c4b3696ec2eeb9c548ef361c1c741677dcc9
zhongshaochen <zhongshaochen@gmail.com>
2016-04-27 01:39:39 +0000

    Make a basic User model (including secure passwords)

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,17 @@
+class StaticPagesController < ApplicationController
+  def home
+    #render template: "static_pages/#{params[:page]}"
+  end
+
+  def help
+   # render template: "static_pages/#{params[:page]}"
+  end
+  
+  def about
+    
+  end
+  
+  def contact
+    
+  end  
+end
